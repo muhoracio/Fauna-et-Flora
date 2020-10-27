ebed11bf687dd7b9cc85c927cf313e12aa2bbf97
muhoracio <65296229+muhoracio@users.noreply.github.com>
2020-10-27 09:54:06 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper(".swiper-container", {
+  spaceBetween: 30,
+  effect: "fade",
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+window.onscroll = function () {
+  headerFunction();
+};
+
+// Get the header
+const header = document.querySelector("#header");
+const logo = document.querySelector("#logo");
+
+function headerFunction() {
+  if (window.pageYOffset > 30) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
